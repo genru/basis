67dcd0e044d5273867cc31a439bfe7faa6fc5e39
chris gao <attic@joro.dreamhost.com>
2013-11-10 09:06:02 -0800

    product env

--- app/propel/sql/default.sql
+++ app/propel/sql/default.sql
@@ -4,6 +4,95 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ---------------------------------------------------------------------
+-- basis_key
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `basis_key`;
+
+CREATE TABLE `basis_key`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `product_key` VARCHAR(255),
+    `product_id` INTEGER NOT NULL,
+    `order_id` INTEGER,
+    `host_id` INTEGER,
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
+    PRIMARY KEY (`id`),
+    INDEX `basis_key_FI_1` (`product_id`),
+    INDEX `basis_key_FI_2` (`order_id`),
+    INDEX `basis_key_FI_3` (`host_id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- basis_product
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `basis_product`;
+
+CREATE TABLE `basis_product`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- basis_customer
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `basis_customer`;
+
+CREATE TABLE `basis_customer`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `email` VARCHAR(255),
+    `organization` VARCHAR(255),
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- basis_order
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `basis_order`;
+
+CREATE TABLE `basis_order`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `agent` VARCHAR(255),
+    `trans_id` VARCHAR(255),
+    `state` TINYINT,
+    `customer_id` INTEGER NOT NULL,
+    `product_id` INTEGER NOT NULL,
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
+    PRIMARY KEY (`id`),
+    INDEX `basis_order_FI_1` (`customer_id`),
+    INDEX `basis_order_FI_2` (`product_id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- basis_host
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `basis_host`;
+
+CREATE TABLE `basis_host`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fingerprint` VARCHAR(255),
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
 -- fos_user
 -- ---------------------------------------------------------------------
 
@@ -28,6 +117,8 @@ CREATE TABLE `fos_user`
     `credentials_expired` TINYINT(1) DEFAULT 0,
     `credentials_expire_at` DATETIME,
     `roles` TEXT,
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `fos_user_U_1` (`username_canonical`),
     UNIQUE INDEX `fos_user_U_2` (`email_canonical`)
@@ -44,6 +135,8 @@ CREATE TABLE `fos_group`
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `roles` TEXT,
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
     PRIMARY KEY (`id`)
 ) ENGINE=MyISAM;
 
@@ -57,6 +150,8 @@ CREATE TABLE `fos_user_group`
 (
     `fos_user_id` INTEGER NOT NULL,
     `fos_group_id` INTEGER NOT NULL,
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
     PRIMARY KEY (`fos_user_id`,`fos_group_id`),
     INDEX `fos_user_group_FI_2` (`fos_group_id`)
 ) ENGINE=MyISAM;
--- src/Exina/AdminBundle/Event/PayPalListener.php
+++ src/Exina/AdminBundle/Event/PayPalListener.php
@@ -4,6 +4,9 @@ namespace Exina\AdminBundle\Event;
 
 use Orderly\PayPalIpnBundle\Event\PayPalEvent;
 use Doctrine\Common\Persistence\ObjectManager;
+use Exina\AdminBundle\Model\Customer;
+use Exina\AdminBundle\Model\Order;
+use Exina\AdminBundle\Model\Host;
 
 class PayPalListener {
 
@@ -14,7 +17,19 @@ class PayPalListener {
     }
 
     public function onIPNReceive(PayPalEvent $event) {
-        $ipn = $event->getIPN();
+        // $ipn = $event->getIPN();
         // do your stuff
+
+       //$customer = new Customer();
+       //$customer->setName("john Doe");
+       //$customer->setEmail("john@pais.com");
+       //$customemr->setOrganization("Paypal Inc.");
+	//$customer->save();
+
+        $p = new Host();
+	$p->setFingerprint("ssss");
+  	$p->save();
+	$response = $event->getResponse();
+	$response->setStatusCode(520, "failed");
     }
 }
